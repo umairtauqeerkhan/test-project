0be698867ca156aa3ea4c5f1e8db5789dc2b6b64
umair khan <umair.tauqeer@healthwire.pk>
2022-09-06 14:00:22 +0500

    images load

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,14 +4,26 @@
 // that code so it'll be compiled.
 
 
-
+require.context('../images', true)
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap"
 import 'css/site.scss'
+import 'css/bootstrap.min.scss'
+import 'css/owl.carousel.min.scss'
+import 'css/select2.min.scss'
 import 'packs/app.min.js'
+import 'packs/bootstrap.min.js'
+import 'packs/jquery.steller.js'
+import 'packs/owl.carousel.min.js'
+import 'packs/popper.min.js'
+import 'packs/select2.min.js'
+document.addEventListener("turbolinks:load", () => {
+    $('[data-toggle="tooltip"]').tooltip();
+    $('[data-toggle="popover"]').popover()
+})
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
